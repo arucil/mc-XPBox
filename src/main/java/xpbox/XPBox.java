1bfc2dfc71d871fa1db2c0346f159212d7476578
Flithor <flithor@live.com>
2017-08-06 16:31:46 +0800

    update to 1.12 port of minecraft

--- src/main/java/xpbox/XPBox.java
+++ src/main/java/xpbox/XPBox.java
@@ -1,12 +1,16 @@
 package xpbox;
 
 import net.minecraft.block.Block;
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemBlock;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.common.capabilities.*;
 import net.minecraftforge.event.AttachCapabilitiesEvent;
@@ -16,14 +20,15 @@ import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.registry.ForgeRegistries;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 
 @Mod(modid = XPBox.MODID, name = XPBox.NAME, version = XPBox.VERSION,
-      acceptedMinecraftVersions = "[1.10.2]")
+      acceptedMinecraftVersions = "[1.12]")
 public class XPBox {
    public static final String MODID = "xpbox";
    public static final String NAME = "XPBox";
-   public static final String VERSION = "1.0";
+   public static final String VERSION = "1.0.1.12";
 
    public static Block blockXPBox;
 
@@ -36,19 +41,21 @@ public class XPBox {
 
    @Mod.EventHandler
    public void preInit(FMLPreInitializationEvent e) {
-      // register block
-      GameRegistry.register(blockXPBox = new BlockXPBox());
-      ItemBlock item = new ItemBlock(blockXPBox);
-      GameRegistry.register(item, blockXPBox.getRegistryName());
-
-      proxy.registerItemRenderer(item);
+	   
+	   //register blockxpbox
+	   ForgeRegistries.BLOCKS.register(blockXPBox=new BlockXPBox().setUnlocalizedName("xpbox"));
+	   
+	   //register itemxpbox
+	   ForgeRegistries.ITEMS.register(new ItemBlock(blockXPBox).setRegistryName(blockXPBox.getRegistryName()));
+	   
+	   proxy.registerItemRenderer(Item.getItemFromBlock(blockXPBox));
 
       // init network
       PacketHandler.init();
    }
 
    @Mod.EventHandler
-   public void init(FMLInitializationEvent e) {
+   public void init(FMLInitializationEvent e) { 
       // register recipe
       BlockXPBox.registerRecipe();
 
@@ -59,12 +66,13 @@ public class XPBox {
 
       // register event handler
       MinecraftForge.EVENT_BUS.register(this);
+      
    }
 
    @SubscribeEvent
-   public void attachCapability(AttachCapabilitiesEvent.Entity e) {
-      if (e.getEntity() instanceof EntityPlayer
-            && !e.getEntity().hasCapability(XP_CAPABILITY, null))
+   public void attachCapability(AttachCapabilitiesEvent<Entity> e) {
+      if (e.getObject() instanceof EntityPlayer
+            && !e.getObject().hasCapability(XP_CAPABILITY, null))
          e.addCapability(new ResourceLocation(MODID, "xpCapability"),
                new XPCapability.Provider());
    }
